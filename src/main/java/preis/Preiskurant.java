1440517a1dde8f3475b7422f4ae5a90b42366dc1
DmitryBuryanov <buryanov.dv@edu.spbstu.ru>
2020-02-09 18:39:39 +0300

    1

--- src/main/java/preis/Preiskurant.java
+++ src/main/java/preis/Preiskurant.java
@@ -1,8 +1,42 @@
 package preis;
 
-public class Preiskurant{
+import java.util.ArrayList;
+
+public class Preiskurant {
+
     public static void main(String[] args) {
-        double i = 24.764;
-        System.out.println(i);
+
     }
+
+    public static ArrayList<Good> put(ArrayList<Good> newList, Good newGood) {
+        newList.add(newGood);
+        return newList;
+    }
+
+    public static ArrayList<Good> changePrice(ArrayList<Good> newList, Good newGood) {
+        for (int i = 0; i < newList.size(); i++) {
+            if (newGood.name == newList.get(i).name) newList.get(i).price = newGood.price;
+        }
+        return newList;
+    }
+
+    public static ArrayList<Good> changeName(ArrayList<Good> newList, Good newGood) {
+        for (int i = 0; i < newList.size(); i++) {
+            if (newGood.code == newList.get(i).code) newList.get(i).name = newGood.name;
+        }
+        return newList;
+    }
+
+    public static ArrayList<Good> delete(ArrayList<Good> newList, Good newGood) {
+        newList.remove(newGood);
+        return newList;
+    }
+
+    public static double checkPrice(ArrayList<Good> newList, int newCode, int count) {
+        for (int i = 0; i < newList.size(); i++) {
+            if (newCode == newList.get(i).code) return newList.get(i).price * count;
+        }
+        return 0.0;
+    }
+
 }
