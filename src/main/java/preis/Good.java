7d79c7ef20a21a1a1c189ac0947536c86ea31ca3
DmitryBuryanov <buryanov.dv@edu.spbstu.ru>
2020-02-09 18:40:42 +0300

    1

---
+++ src/main/java/preis/Good.java
@@ -0,0 +1,13 @@
+package preis;
+
+public class Good {
+    String name;
+    int code;
+    double price;
+
+    Good(String name, int code, double price) {
+        this.name = name;
+        this.code = code;
+        this.price = price;
+    }
+}
